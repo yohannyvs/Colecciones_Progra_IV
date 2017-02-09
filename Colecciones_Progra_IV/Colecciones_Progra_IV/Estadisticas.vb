0384b1f1ee1a4ec5e5a87d0cbddcd25760de69c6
yohannyvs <yohannyvs@gmail.com>
2017-02-09 13:45:50 -0600

    Fichas

---
+++ Colecciones_Progra_IV/Colecciones_Progra_IV/Estadisticas.vb
@@ -0,0 +1,3 @@
+﻿Public Class Estadisticas
+
+End Class
--- Colecciones_Progra_IV/Colecciones_Progra_IV/funciones.vb
+++ Colecciones_Progra_IV/Colecciones_Progra_IV/funciones.vb
@@ -1,10 +1,52 @@
-﻿Public Class funciones
+﻿Imports System.Threading
+Imports System.Collections
+Public Class funciones
 
-    Sub ing_cliente()
+    Dim Ficha As String
+    Dim caja_estado As Boolean
+    Dim cp As Integer = 1
+    Dim cn As Integer = 1
+    Dim psp As Integer = 1
+    Dim psn As Integer = 1
+    Dim client As List(Of funciones)
+    Dim f As New funciones
+    Dim tiempo As Integer = CInt(Int((20 * Rnd()) + 1)) 'Crea ramdom para los tiempos
+
+    Sub Fichas(area As String, cliente As String) 'crea el tipo de ficha
+
+        If (area = "Cajas" And cliente = "Preferencial") Then
+            Ficha = "CP - " + cp
+            cp += 1
+        End If
+
+        If (area = "Cajas" And cliente = "Normal") Then
+            Ficha = "CN - " + cn
+            cn += 1
+        End If
+
+        If (area = "Pataforma de Servicios" And cliente = "Preferencial") Then
+            Ficha = "PSP - " + psp
+            psp += 1
+        End If
+
+        If (area = "Pataforma de Servicios" And cliente = "Normal") Then
+            Ficha = "PSN - " + psn
+            psn += 1
+        End If
 
     End Sub
 
-    Sub aten_cliente()
+    Sub cajas()
+        If (caja_estado = True) Then
+
+        End If
+    End Sub
+
+    Sub simulacion()
+
+    End Sub
+
+    Sub estadisticas()
 
     End Sub
 
